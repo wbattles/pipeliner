dc12c6a3ccfd38ccdd56c56a8fcdc773c26ddb89
wbattles <wileybattles@gmail.com>
2025-02-04 20:21:03 -0600

    fix pipeline path

--- terraform/enviroment/main.tf
+++ terraform/enviroment/main.tf
@@ -9,8 +9,8 @@ module "my_ec2" {
 
   instance_name   = var.instance_name
   instance_type   = var.instance_type
-  # ami_id          = var.ami_id
-  ami_id          = data.aws_ami.amazon_linux_2.id
+  ami_id          = var.ami_id
+  # ami_id          = data.aws_ami.amazon_linux_2.id
   vpc_id          = var.vpc_id
   subnet_id       = var.subnet_id
 }
