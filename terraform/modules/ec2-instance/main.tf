e52d58700f430db114304e13863c6b66b3d4e295
wbattles <wileybattles@gmail.com>
2025-02-07 14:25:01 -0600

    update egress rule

--- terraform/modules/ec2-instance/main.tf
+++ terraform/modules/ec2-instance/main.tf
@@ -23,8 +23,8 @@ resource "aws_vpc_security_group_ingress_rule" "web" {
   security_group_id = aws_security_group.public_traffic.id
   cidr_ipv4         = "0.0.0.0/0"
 
-  from_port   = 80
   ip_protocol = "tcp"
+  from_port   = 80
   to_port     = 80
 }
 
@@ -32,8 +32,8 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = aws_security_group.public_traffic.id
   cidr_ipv4         = "0.0.0.0/0"
 
-  from_port   = 22
   ip_protocol = "tcp"
+  from_port   = 22
   to_port     = 22
 }
 
@@ -41,9 +41,9 @@ resource "aws_vpc_security_group_egress_rule" "web" {
   security_group_id = aws_security_group.public_traffic.id
   cidr_ipv4         = "0.0.0.0/0"
 
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
+  ip_protocol = -1
+  from_port   = -1
+  to_port     = -1
 }
 
 resource "aws_eip" "instance_eip" {
