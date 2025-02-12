3a25413d8f77e6e54e08afc198ef3681131a31ab
wbattles <wileybattles@gmail.com>
2025-02-11 18:52:31 -0600

    try docker stop on name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,16 +30,9 @@ sudo ./aws/install --update
 aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
 
 
-sudo kill -9 $(sudo lsof -ti:8000) || true
+sudo docker stop run_app || true
+sudo docker rm run_app || true
 
-CONTAINER_ID=$(sudo docker ps -q --filter "publish=8000")
-if [ ! -z "$CONTAINER_ID" ]; then
-    sudo docker stop $CONTAINER_ID
-    sudo docker rm $CONTAINER_ID
-fi
-
-sudo netstat -tuln | grep :8000 && sudo fuser -k 8000/tcp || true
-sudo docker system prune -f
 
 sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ECR_REPOSITORY:latest
 sudo docker run -d \
