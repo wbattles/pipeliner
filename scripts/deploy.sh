a92d29a6b54fd46dfd51bf31ec6141c8b1e23d19
wbattles <wileybattles@gmail.com>
2025-02-11 18:46:47 -0600

    add port clearing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,17 +29,22 @@ sudo ./aws/install --update
 
 aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
 
-sudo docker stop run_app || true
-sudo docker rm -f run_app || true
 
-while ss -tuln | grep :8000; do
-    sudo lsof -ti:8000 | xargs -r sudo kill -9
-    sleep 5
-done
+sudo kill -9 $(sudo lsof -ti:8000) || true
+
+CONTAINER_ID=$(sudo docker ps -q --filter "publish=8000")
+if [ ! -z "$CONTAINER_ID" ]; then
+    sudo docker stop $CONTAINER_ID
+    sudo docker rm $CONTAINER_ID
+fi
+
+sudo netstat -tuln | grep :8000 && sudo fuser -k 8000/tcp || true
+sudo docker system prune -f
 
 sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ECR_REPOSITORY:latest
 sudo docker run -d \
   -p 8000:8000 \
   --env AWS_EC2_METADATA_DISABLED=false \
   --name run_app \
+  --network host \
   $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ECR_REPOSITORY:latest
